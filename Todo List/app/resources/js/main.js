088edb723159a65cf576f165918fc5c3676e8edc
Msarbel <35122452+msarbel@users.noreply.github.com>
2018-06-04 20:32:31 -0400

    Updating comments

--- Todo List/app/resources/js/main.js
+++ Todo List/app/resources/js/main.js
@@ -1,6 +1,6 @@
 
 
-
+//Get locally stored object list or use new empty version
 var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
   todo: [],
   complete: []
@@ -20,9 +20,10 @@ var completeSVG = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:x
 viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8
     c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>`
 
+// render after svg variables created
 renderTodoList();
 
-
+// on button click add input to list
 document.getElementById('add').addEventListener('click', function() {
   var value = document.getElementById('item').value;
   if(value) {
@@ -30,7 +31,7 @@ document.getElementById('add').addEventListener('click', function() {
   }
 });
 
-
+// on enter key stroke add input to list
 document.getElementById('item').addEventListener('keydown', function(e) {
   var value = this.value;
   if (e.code === 'Enter' && value) {
@@ -51,6 +52,7 @@ function addItem(value) {
 function renderTodoList() {
   if(!data.todo.length && !data.completed.length) return;
 
+// loop through to render both lists if applicable 
 for (var i = 0; i < data.todo.length; i++) {
   var value = data.todo[i];
   addItemTodo(value);
